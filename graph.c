f871e5e2981a5d5091e6c9aef27f82b4ece87a6f
TaeHyun Park <pthkuku99@gmail.com>
2022-05-26 11:19:38 +0900

    template

---
+++ graph.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+
+    printf("---------------[2018068040]--------[Park Taehyun]---------------\n");
+    printf("-------------------------Graph Searches-------------------------\n");
+    printf("----------------------------------------------------------------\n");
+    printf(" Initialize Graph   = z\n");
+    printf(" Insert Vertex      = v         Insert Edge             = e\n");
+    printf(" Depth Frist Search = d         Breath First Search     = b\n");
+    printf(" Print Graph        = p         Quit                    = q\n");
+    printf("----------------------------------------------------------------\n");
+
+    return;
+}
